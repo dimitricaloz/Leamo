cc4eff651e2c6c2c28e351a6b5271ff068cc681f
dimitricaloz <dimitri.caloz@studentfr.ch>
2023-05-10 12:19:23 +0200

    midi 10.05.2023

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,22 @@ import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
 
 const swiper = new Swiper(".swiper-container", {
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
   },
   navigation: {
     nextEl: ".carousel-next",
     prevEl: ".carousel-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
   },
 });
